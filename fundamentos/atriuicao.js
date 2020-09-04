a7386c85fbe51153f89e43c270534c6add7a8de3
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-09-04 20:59:58 -0300

    Atribuição

---
+++ fundamentos/atriuicao.js
@@ -0,0 +1,19 @@
+const a = 7
+let  b = 3
+
+
+b += a // atribuição aditiva.  está acrecentando o valor de a ha variavel b
+console.log(b)
+
+
+b-= 4 // b = b - 4
+console.log(b)
+
+
+b *= 4 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2 
+console.log(b)
+
+b %=2 // b = b % 2
