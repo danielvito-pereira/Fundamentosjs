c427eae9ec42c416830ddd3a90da81c41cda1bac
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-09-14 19:59:29 -0300

    ternario

---
+++ fundamentos/ternario.js
@@ -0,0 +1,4 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' :'Reprovado'//ternario tem tres a primeira é sempre uma expreção  que retorna verdadeoro ou falso depois vem ? a interogação 
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
