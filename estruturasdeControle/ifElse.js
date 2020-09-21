04342357c36013cbe39f807851d43cfe29691901
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-09-21 09:52:41 -0300

    if else if

---
+++ estruturasdeControle/ifElse.js
@@ -0,0 +1,11 @@
+const impriminrResultado = function(nota) {
+    if(nota >= 7) {
+        console.log('Arpovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+impriminrResultado(10)
+impriminrResultado(4)
+impriminrResultado('Epa!')//cuidado
