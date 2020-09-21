8a02978c8736a5c74e8774b5a0c3db055159f1aa
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-09-21 10:40:07 -0300

    for

---
+++ estruturasdeControle/for.js
@@ -0,0 +1,18 @@
+let contador = 1
+
+while(contador <= 10) {
+    console.log(`contador ${contador}`)
+    contador++
+}
+
+
+
+for (let i =1; i <= 10; i++)  {
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for(let i =0; i < notas.length; i++){//length mede o comprimento do array. está dizendo que começar do zero e ira até um valor menor que o do array 
+    console.log(`nota= ${notas[i]}`)
+}
+
