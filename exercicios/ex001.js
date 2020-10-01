59aaef23d53c26ef632d99ce16f82be11cb758a7
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-10-01 10:30:16 -0300

    0001

---
+++ exercicios/ex001.js
@@ -0,0 +1,9 @@
+let n1, n2
+n1 = prompt('Digite um valor para ser somado')
+n2 = prompt('Digite o segundo valor para ser somado')
+
+function soma(s) {
+    s=n1+n2
+    return  alert ("O valor digitado maior que 5 foi: " +s)
+}
+
