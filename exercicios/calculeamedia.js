2a4a17c402692a1c3c51bcde875e41502ed44b6b
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-10-20 18:38:03 -0300

    exercicios

---
+++ exercicios/calculeamedia.js
@@ -0,0 +1,13 @@
+
+
+let n1, n2, n3, n4
+
+
+
+n1 = prompt("Digite a primeira nota : ")
+n2 = prompt("Digite a primeira nota : ")
+n3 = prompt("Digite a primeira nota : ")
+n4 = prompt("Digite a primeira nota : ")
+
+media = n1 + n2 + n3 + n4 / 4
+alert("O resultado é :" + media())
