bf815daaf318ba0e584ae21242d1997008973ac1
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-10-23 14:47:55 -0300

    01

--- exercicios/oficiais/01.js
+++ exercicios/oficiais/01.js
@@ -1,11 +1,11 @@
-function doisvalores() {
-     soma = 2+8;
-     sub= 2-8;
-     mult = 2*8
-     div=2/8
-     return console.log(`O resultados são ${soma}, dá subtração ${sub}, dá multiplicação ${mult}, dá divisão ${div} `)
-
+function doisvalores(a,b) {
+     soma= a+b
+     div=a/b
+     mult = a*b
+     sub=a-b
+     calculos= soma,div,mult,sub
+     return calculos
 }
 
 
-console.log(doisvalores())
+console.log(22,44)
