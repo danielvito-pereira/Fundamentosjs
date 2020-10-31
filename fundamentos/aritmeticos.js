db70dc55e672080d31a2494c464f4bbea81b004c
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-10-31 11:27:17 -0300

    ;

--- fundamentos/aritmeticos.js
+++ fundamentos/aritmeticos.js
@@ -1,8 +1,8 @@
-const [a,b,c,d] = [3,5,1,15]
-const soma = a + b + c + d
-const subtracao = d - b
-const multiplicacao = a * b
-const divisao = d / a
-const modulo = a % 2//usado pra saber se e prou ipar
+const [a,b,c,d] = [3,5,1,15];
+const soma = a + b + c + d;
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+const modulo = a % 2;//usado pra saber se e prou ipar
 
-console. log (soma, subtracao, multiplicacao, divisao, modulo)
+console. log (soma, subtracao, multiplicacao, divisao, modulo);
